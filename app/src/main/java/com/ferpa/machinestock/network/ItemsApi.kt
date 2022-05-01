0a19b7cbf72ba251cd0a31f6e3897120fd0686b7
Fernando Pailhe <ferpai@gmail.com>
2022-05-01 11:59:30 -0300

    Initial commit

--- app/src/main/java/com/ferpa/machinestock/network/ItemsApi.kt
+++ app/src/main/java/com/ferpa/machinestock/network/ItemsApi.kt
@@ -1,18 +1,32 @@
 package com.ferpa.machinestock.network
 
 import com.ferpa.machinestock.model.Item
-import retrofit2.http.GET
+import okhttp3.MultipartBody
+import retrofit2.Call
+import retrofit2.http.*
 
 interface ItemsApi {
 
-    @GET("/testItem")
-    suspend fun getTestItem(): Item
+    @GET("/getItemById")
+    suspend fun getItem(@Body item: Item) : Item
 
+    @GET("/getAllItems")
+    suspend fun getItems(@Query ("product") product: String, @Query ("value") value: String) : List<Item>
+
+    @POST("/newItem")
+    suspend fun postNewItem(@Body item: Item): Call<*>
+
+    @POST("/updateItem")
+    suspend fun updateItem(@Body item: Item): Call<*>
+
+    @Multipart
+    @POST
+    suspend fun postNewPhoto(@Part image:MultipartBody.Part):Call<*>
 
     companion object {
-        const val BASE_URL = "http://192.168.100.4:8080"
+        const val BASE_LOCAL_URL = "http://192.168.100.4:8080"
+        const val BASE_ORACLE_URL = "http://129.151.106.230"
     }
-
 
 
 }
