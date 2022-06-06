e21c2b944293923a92102a86561421c451cc3445
Fernando Pailhe <ferpai@gmail.com>
2022-06-06 16:00:00 -0300

    Initial commit

--- app/src/main/java/com/ferpa/machinestock/MainActivity.kt
+++ app/src/main/java/com/ferpa/machinestock/MainActivity.kt
@@ -3,6 +3,7 @@ package com.ferpa.machinestock
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import dagger.hilt.android.AndroidEntryPoint
@@ -15,6 +16,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContentView(R.layout.activity_main)
 
         // Retrieve NavController from the NavHostFragment
