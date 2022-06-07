dbe4cbbe071d2937f720fa97e67b5ddf45795597
Fernando Pailhe <ferpai@gmail.com>
2022-06-07 12:00:16 -0300

    Initial commit

--- app/src/main/java/com/ferpa/machinestock/network/ItemsApi.kt
+++ app/src/main/java/com/ferpa/machinestock/network/ItemsApi.kt
@@ -3,15 +3,19 @@ package com.ferpa.machinestock.network
 import com.ferpa.machinestock.model.Item
 import okhttp3.MultipartBody
 import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.*
 
 interface ItemsApi {
 
     @GET("/getItemById")
-    suspend fun getItem(@Body item: Item) : Item
+    suspend fun getItemById(@Body itemId: Long): Item
 
     @GET("/getAllItems")
     suspend fun getItems(@Query ("product") product: String, @Query ("value") value: String) : List<Item>
+
+    @GET("/getAllItems")
+    suspend fun getAllItems(): Response<List<Item>>
 
     @POST("/newItem")
     suspend fun postNewItem(@Body item: Item): Call<*>
