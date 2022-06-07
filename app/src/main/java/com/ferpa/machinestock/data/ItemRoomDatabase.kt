d0fe066fccb4a08433ea0dc38353c5bd71efa8dc
Fernando Pailhe <ferpai@gmail.com>
2022-06-07 12:01:32 -0300

    Initial commit

--- app/src/main/java/com/ferpa/machinestock/data/ItemRoomDatabase.kt
+++ app/src/main/java/com/ferpa/machinestock/data/ItemRoomDatabase.kt
@@ -4,11 +4,10 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.ferpa.machinestock.model.Item
 
-@Database (entities = [Item::class], version = 1, exportSchema = true)
+@Database (entities = [Item::class], version = 1, exportSchema = false)
 abstract class ItemRoomDatabase : RoomDatabase () {
 
     abstract fun itemDao(): ItemDao
-
 
     companion object {
         const val DATABASE_NAME = "used_item_database"
