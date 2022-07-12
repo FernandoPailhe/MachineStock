d7a7ecc7c4a73ee53e4f7b98169a7a8ed97238d8
Fernando Pailhe <ferpai@gmail.com>
2022-07-12 17:06:41 -0300

    New const values

--- app/src/main/java/com/ferpa/machinestock/utilities/Const.kt
+++ app/src/main/java/com/ferpa/machinestock/utilities/Const.kt
@@ -3,11 +3,11 @@ package com.ferpa.machinestock.utilities
 object Const {
 
 
-    //const val OWNER_1 = "Dueño 1"
-    //const val OWNER_2 = "Dueño 2"
+    const val OWNER_1 = "Dueño 1"
+    const val OWNER_2 = "Dueño 2"
 
-    //const val LOCATION_1 = "Depósito 1"
-    //const val LOCATION_2 = "Depósito 2"
+    const val LOCATION_1 = "Depósito 1"
+    const val LOCATION_2 = "Depósito 2"
 
     const val MENU_LIST_SIZE = 5
 
@@ -15,12 +15,4 @@ object Const {
     const val USED_MACHINES_PHOTO_BASE_URL = "usedmachinesphotos"
 
     const val MAX_IMAGE_SIZE: Long = 1036800
-
-    const val OWNER_1 = "Maquimundo"
-    const val OWNER_2 = "Canavese"
-    const val SOCIEDAD = "Sociedad"
-
-    const val LOCATION_1 = "Zoilo"
-    const val LOCATION_2 = "Canavese"
-
 }
