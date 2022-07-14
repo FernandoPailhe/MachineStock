ff691cfc64b5a2543f606b9da8a0d083cb0aa8ad
Fernando Pailhe <ferpai@gmail.com>
2022-07-14 12:17:49 -0300

    Update Constant

--- app/src/main/java/com/ferpa/machinestock/utilities/Const.kt
+++ app/src/main/java/com/ferpa/machinestock/utilities/Const.kt
@@ -3,20 +3,19 @@ package com.ferpa.machinestock.utilities
 object Const {
 
 
+    const val LOCATION_1 = "Depósito 1"
+    const val LOCATION_2 = "Depósito 2"
+
     const val OWNER_1 = "Dueño 1"
     const val OWNER_2 = "Dueño 2"
     const val SOCIEDAD = "Sociedad"
-
-    const val LOCATION_1 = "Depósito 1"
-    const val LOCATION_2 = "Depósito 2"
 
     const val MENU_LIST_SIZE = 5
 
     const val FIRESTORAGE_BASE_URL = "gs://machinestock.appspot.com/"
     const val USED_MACHINES_PHOTO_BASE_URL = "usedmachinesphotos"
 
-    const val MAX_IMAGE_SIZE: Long = 1036800
-
-
+    const val REQUEST_GALLERY_PHOTO = 199
+    const val REQUEST_TAKE_PHOTO = 198
 
 }
